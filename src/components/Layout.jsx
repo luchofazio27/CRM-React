4e67ff639e1c95bc69461543927b33d062083045
luchofazio27 <luchofazio@gmail.com>
2023-05-14 17:19:59 -0300

    Creacion de componente Formulario

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
-import { Outlet, Link, useLocation } from "react-router-dom"
+import { Outlet, NavLink, useLocation } from "react-router-dom"
 
-const Layout = () => {
+function Layout() {
 
     const location = useLocation()
 
@@ -10,14 +10,20 @@ const Layout = () => {
       <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
         <h2 className="text-4xl font-black text-center text-white">CRM - Clientes</h2>
 
-        <nav className="mt-10">
-        <Link 
-        className={`${location.pathname === '/' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300 text-white`}
-        to="/">Clientes</Link>
-        <Link 
-        className={`${location.pathname === '/clientes/nuevo' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300 text-white`} 
-        to="/clientes/nuevo">Nuevo Cliente</Link>
-      </nav>
+        <nav className="mt-10"> 
+         <NavLink
+            className={({ isActive }) => `${ isActive ? "text-blue-400" : "text-white" } text-2xl block mt-2 hover:text-blue-300` }
+            to="/" >
+            Inicio
+          </NavLink>
+ 
+          <NavLink
+            className={({ isActive }) => `${ isActive ? "text-blue-400" : "text-white" } text-2xl block mt-2 hover:text-blue-300` }
+            to="/clientes/nuevo" >
+            Nuevo cliente
+          </NavLink>
+        </nav>
+
       </aside>
 
       <main className="md:w-3/4 p-10 md:h-screen overflow-scroll">
